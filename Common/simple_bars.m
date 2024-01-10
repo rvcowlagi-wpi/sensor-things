cb059d4e91fb5071e55e6ce817f0d23ebb0f73ba
rvcowlagi-wpi <129526829+rvcowlagi-wpi@users.noreply.github.com>
2024-01-10 17:26:05 -0500

    Jelly bean PMF bars

--- Common/simple_bars.m
+++ Common/simple_bars.m
@@ -1,30 +1,75 @@
+%% 
 clear variables; close all; clc;
 
+fontSize_ = 50;
 figure('units', 'normalized', 'OuterPosition', [0.05 0.05 0.8 0.9]);
 bar([0 1], [0.5 0.5], 'EdgeColor','white', 'FaceColor','black', ...
 	'FaceAlpha',0.6, 'BarWidth',0.1)
 xlabel('Outcome $x$', 'FontName', 'Times New Roman', ...
-	'FontSize', 20, 'FontWeight', 'bold', 'interpreter', 'latex');
+	'FontSize', fontSize_, 'FontWeight', 'bold', 'interpreter', 'latex');
 ylabel('$F_X(x)$', 'FontName', 'Times New Roman', ...
-	'FontSize', 20, 'FontWeight', 'bold', 'interpreter', 'latex');
+	'FontSize', fontSize_, 'FontWeight', 'bold', 'interpreter', 'latex');
 ylim([0 1])
 ax = gca;
 ax.FontName = 'Times New Roman';
-ax.FontSize = 20;
+ax.FontSize = fontSize_;
 
 exportgraphics(ax, 'coin-toss.png', 'Resolution', 300);
 
+%%
+clear variables; close all; clc;
+
+fontSize_ = 50;
 
 figure('units', 'normalized', 'OuterPosition', [0.05 0.05 0.8 0.9]);
 bar(1:6, (1/6)*ones(1,6), 'EdgeColor','white', 'FaceColor','black', ...
-	'FaceAlpha', 0.6, 'BarWidth',0.1)
+	'FaceAlpha', 0.6, 'BarWidth',0.3)
 xlabel('Outcome $x$', 'FontName', 'Times New Roman', ...
-	'FontSize', 20, 'FontWeight', 'bold', 'interpreter', 'latex');
+	'FontSize', fontSize_, 'FontWeight', 'bold', 'interpreter', 'latex');
 ylabel('$F_X(x)$', 'FontName', 'Times New Roman', ...
-	'FontSize', 20, 'FontWeight', 'bold', 'interpreter', 'latex');
+	'FontSize', fontSize_, 'FontWeight', 'bold', 'interpreter', 'latex');
 ylim([0 1])
 ax = gca;
 ax.FontName = 'Times New Roman';
-ax.FontSize = 20;
+ax.FontSize = fontSize_;
 
 exportgraphics(ax, 'die-toss.png', 'Resolution', 300);
+
+%%
+clear variables; close all; clc;
+
+fontSize_ = 50;
+figure('units', 'normalized', 'OuterPosition', [0.05 0.05 0.8 0.9]);
+bar(350:10:410, [0.05 0.1 0.2 0.3 0.2 0.1 0.05], ...
+	'EdgeColor','white', 'FaceColor','black', ...
+	'FaceAlpha', 0.6, 'BarWidth', 0.3)
+xlabel('Outcome $x$', 'FontName', 'Times New Roman', ...
+	'FontSize', fontSize_, 'FontWeight', 'bold', 'interpreter', 'latex');
+ylabel('$F_X(x)$', 'FontName', 'Times New Roman', ...
+	'FontSize', fontSize_, 'FontWeight', 'bold', 'interpreter', 'latex');
+ylim([0 0.4])
+ax = gca;
+ax.FontName = 'Times New Roman';
+ax.FontSize = fontSize_;
+
+exportgraphics(ax, 'pmf-bean1.png', 'Resolution', 300);
+
+
+%%
+clear variables; close all; clc;
+
+fontSize_ = 50;
+figure('units', 'normalized', 'OuterPosition', [0.05 0.05 0.8 0.9]);
+bar(350:10:410, [0.1 0.15 0.15 0.2 0.15 0.15 0.1], ...
+	'EdgeColor','white', 'FaceColor','black', ...
+	'FaceAlpha', 0.6, 'BarWidth', 0.3)
+xlabel('Outcome $x$', 'FontName', 'Times New Roman', ...
+	'FontSize', fontSize_, 'FontWeight', 'bold', 'interpreter', 'latex');
+ylabel('$F_X(x)$', 'FontName', 'Times New Roman', ...
+	'FontSize', fontSize_, 'FontWeight', 'bold', 'interpreter', 'latex');
+ylim([0 0.4])
+ax = gca;
+ax.FontName = 'Times New Roman';
+ax.FontSize = fontSize_;
+
+exportgraphics(ax, 'pmf-bean2.png', 'Resolution', 300);
--- Common/uscrn_temperature.m
+++ Common/uscrn_temperature.m
@@ -20,30 +20,32 @@ temp5yr		= [temp2019; temp2020; temp2021; temp2022; temp2023];
 
 temp5yr(temp5yr == -9999) = [];
 
+
+fontSize_ = 50;
 figure('units', 'normalized', 'OuterPosition', [0.05 0.05 0.8 0.9]);
 histogram(temp5yr, 40, 'EdgeColor', 'white', 'FaceColor','black')
 xlabel('Temperature ($^\circ$C)', 'FontName', 'Times New Roman', ...
-	'FontSize', 20, 'FontWeight', 'bold', 'interpreter', 'latex');
+	'FontSize', fontSize_, 'FontWeight', 'bold', 'interpreter', 'latex');
 ylabel('No. of occurences', 'FontName', 'Times New Roman', ...
-	'FontSize', 20, 'FontWeight', 'bold', 'interpreter', 'latex');
+	'FontSize', fontSize_, 'FontWeight', 'bold', 'interpreter', 'latex');
 ax = gca;
 ax.FontName = 'Times New Roman';
-ax.FontSize = 20;
+ax.FontSize = fontSize_;
 
 exportgraphics(ax, 'uscrn-temp-histogram.png', 'Resolution', 300);
 
 close all;
 
 figure('units', 'normalized', 'OuterPosition', [0.05 0.05 0.8 0.9]);
-histogram(temp5yr-273.16, 40, 'EdgeColor', 'black', ...
+histogram(temp5yr+273.16, 40, 'EdgeColor', 'black', ...
 	'DisplayStyle','stairs', 'Normalization','probability', 'LineWidth', 2)
 xlabel('Temperature ($^\circ$K)', 'FontName', 'Times New Roman', ...
-	'FontSize', 20, 'FontWeight', 'bold', 'interpreter', 'latex');
+	'FontSize', fontSize_, 'FontWeight', 'bold', 'interpreter', 'latex');
 ylabel('$f_X(x)$', 'FontName', 'Times New Roman', ...
-	'FontSize', 20, 'FontWeight', 'bold', 'interpreter', 'latex');
-ylim([0 1])
+	'FontSize', fontSize_, 'FontWeight', 'bold', 'interpreter', 'latex');
+ylim([0 0.3])
 ax = gca;
 ax.FontName = 'Times New Roman';
-ax.FontSize = 20;
+ax.FontSize = fontSize_;
 
 exportgraphics(ax, 'uscrn-temp-pdf.png', 'Resolution', 300);
