951a2e3addfa750d3bd5a6a247fda0e0f3014c01
rvcowlagi-wpi <129526829+rvcowlagi-wpi@users.noreply.github.com>
2024-03-06 10:30:42 -0500

    PSD stuff

--- Common/psd_1.m
+++ Common/psd_1.m
@@ -1,9 +1,13 @@
-clear variables; close all; clc
+function psd_1()
+
+close all; clc
 
 fSample = 1e3;
-t_		= 0:1/fSample:1-1/fSample;
-% x = 0*cos(2*pi*100*t) + 5*randn(size(t));
-x_		= cos(2*pi*10*t_) + sin(2*pi*100*t_);
+time_		= 0:1/fSample:1-1/fSample;
+% xSignal_	= cos(2*pi*10*t_) + sin(2*pi*100*t_)
+xSignal_	= randn(size(time_));
+[~, y1]		= ode45(@(t,x) simpleLTI(t,x, xSignal_), time_, 0);	
+ySignal_	= y1(:, 1)'; 
 
 % fs1 = 10;
 % t1 = 0:1/fs1:1;
@@ -11,15 +15,48 @@ x_		= cos(2*pi*10*t_) + sin(2*pi*100*t_);
 % x1 = randn(size(t1));
 % x_ = interp1(t1,x1,t_,"nearest");
 
-N		= length(x_);
-xDFT	= fft(x_);
+N		= length(ySignal_);
+xDFT	= fft(ySignal_);
 xDFT	= xDFT(1:N/2+1);
 psdx	= (1/(fSample*N)) * abs(xDFT).^2;
 psdx(2:end-1) = 2*psdx(2:end-1);
-freq = 0:fSample/length(x_):fSample/2;
+freq = 0:fSample/length(ySignal_):fSample/2;
 
-plot(freq,pow2db(psdx))
-grid on
-title("Periodogram Using FFT")
-xlabel("Frequency (Hz)")
-ylabel("Power/Frequency (dB/Hz)")
+% fig0	= figure;
+% plot(time_, xSignal_, 'LineWidth', 3); grid on;
+% ax0		= gca;
+% make_nice_figures(fig0, ax0, 18, [], ...
+% 	'Time (s)', 'Signal $x(t)$', 'Signal', ...
+% 	[], 'psd_noise_signal.png', [], [-3 3])
+% 
+% fig1	= figure;
+% plot(freq, pow2db(psdx), 'LineWidth', 3); grid on;
+% ax1		= gca;
+% make_nice_figures(fig1, ax1, 18, [], ...
+% 	'Frequency (Hz)', 'Power/Frequency (dB/Hz)', 'Periodogram', ...
+% 	[], 'psd_noise_pdg.png', [], [-350 0])
+
+fig0	= figure;
+plot(time_, ySignal_, 'LineWidth', 3); grid on;
+ax0		= gca;
+make_nice_figures(fig0, ax0, 18, [], ...
+	'Time (s)', 'Signal $y(t)$', 'Signal', ...
+	[], 'psd_preshaped2_noise_signal.png', [], [])
+
+fig1	= figure;
+plot(freq, pow2db(psdx), 'LineWidth', 3); grid on;
+ax1		= gca;
+make_nice_figures(fig1, ax1, 18, [], ...
+	'Frequency (Hz)', 'Power/Frequency (dB/Hz)', 'Periodogram', ...
+	[], 'psd_preshaped2_noise_pdg.png', [], [])
+
+
+	function xDot_ = simpleLTI(t_, x_, u_)
+		tIndex	= max(1, min(round(t_*fSample), length(time_)));
+		ut_		= u_(tIndex);
+
+% 		xDot_	= [0 1; -4 -5]*x_ + [0; 1]*ut_;
+		xDot_	= -2*x_ + ut_; % Brown noise
+	end
+
+end
--- make_nice_figures.m
+++ make_nice_figures.m
@@ -1,12 +1,38 @@
-function make_nice_figures(fig_, ax_, fontSize_, xLabel_, yLabel_, figTitle_, figPosition_)
+function make_nice_figures(fig_, ax_, fontSize_, axTitle_, ...
+	xLabel_, yLabel_, figTitle_, figPosition_, exportAs_, xLim_, yLim_)
 
 fig_.Units		= 'normalized';
-f
-fig_.Position	= [0.1 0.1 0.5*[1 1]];
+if numel(figTitle_)
+	fig_.Name		= figTitle_;
+end
+if numel(figPosition_)
+	fig_.Position	= figPosition_;
+else
+	fig_.Position	= [0.1 0.1 0.5*[1 1]];
+end
 
-figure('Units','normalized', ) 
-subplot(211); hold on; plot(time_stamps, storeXHat(1,:), 'LineWidth', 2)
-title('State estimate mean $\hat{x}_1$ (position)', 'Interpreter', 'latex', 'FontSize', 14)
-xlabel('Time (s)', 'Interpreter', 'latex', 'FontSize', fontSize_);
-ylabel('$\hat{x}_1$ (m)', 'Interpreter', 'latex', 'FontSize', fontSize_);
-ax = gca; ax.FontSize = fontSize_; ax.FontName = 'Times New Roman';
+ax_.FontSize	= fontSize_; 
+ax_.FontName	= 'Times New Roman';
+ax_.TickLabelInterpreter = 'latex';
+if numel(axTitle_)
+	ax_.Title		= axTitle_;
+end
+if numel(xLabel_)
+	ax_.XLabel.String = xLabel_;
+	ax_.XLabel.Interpreter = 'latex';
+end
+if numel(yLabel_)
+	ax_.YLabel.String = yLabel_;
+	ax_.YLabel.Interpreter = 'latex';
+end
+if numel(xLim_)
+	ax_.XLim = xLim_;
+end
+if numel(yLim_)
+	ax_.YLim = yLim_;
+end
+
+
+if numel(exportAs_)
+	exportgraphics(fig_, exportAs_, 'Resolution',150);
+end
