b1346f90cedd2446c35b2b0dedd9175ac8146c50
rvcowlagi-wpi <129526829+rvcowlagi-wpi@users.noreply.github.com>
2024-03-25 15:58:34 -0400

    Update speed_radar.m

--- Assignments/speed_radar.m
+++ Assignments/speed_radar.m
@@ -2,73 +2,90 @@ function speed_radar()
 
 close all; clc
 
-% Measurement error has speed-dependent variance
+%% Constants
+lambda_ = 1/50;			% (km/hr)^-1, inv. mean of prior
+alpha_	= 0.1;			% std. dev. is alpha_*x
+infty_	= 1000;			% Practically infinite
 
-r_	= 5^2; % (km/hr)^2
-xSS	= linspace(0, 400, 100);
-ySS	= linspace(-50, 400, 100);
+%% Posterior
 
-infty = 1000;	% Practical "infinity"
+z1		= 40;			% km/hr
+x1		= 50;			% km/hr
+ProbX	= integral(@(x) posterior_X(x, z1), x1, infty_);					% P[X >= 50]
 
-[xMesh, yMesh]	= meshgrid(xSS, ySS);
-
-fXPrior			= prior_X(xSS);
-fYGivenX		= conditional_YGivenX(xSS(100), ySS);
-fJoint			= joint_XY(xMesh, yMesh);
-
-zStar			= 50;
-xStar			= 60;
-integral(@(x) posterior_XGivenY(x, zStar), xStar, infty)
-% This is P[ X >= xBar | y = yMeas ]
+extAP	= roots([lambda_ 0 100*z1 -100*z1^2]);
+xMAP	= real(extAP(3))
+xLSEE	= integral(@(x) x.*posterior_X(x, z1), 0, infty_)
+xLinLSEE= (1 / lambda_) + ( (1 / lambda_^2) / (alpha_/lambda_ + 1 / lambda_^2) ) * (z1 - (1 / lambda_))
 
 
-% Sanity check; these should all be 1
+%% Plots
+figure;
+xPlot	= linspace(0, 100, 1000);
+plot(xPlot, prior_X(xPlot), 'LineWidth', 2); hold on;
+plot(xPlot, posterior_X(xPlot, z1), 'LineWidth', 2);
+zPlot	= linspace(0, 1.1*max(posterior_X(xPlot, z1)), 1000);
+plot(z1*ones(size(xPlot)), zPlot, 'LineWidth', 2, 'LineStyle','--')
+plot(xMAP*ones(size(xPlot)), zPlot, 'LineWidth', 2, 'LineStyle','--')
+plot(xLSEE*ones(size(xPlot)), zPlot, 'LineWidth', 2, 'LineStyle','--')
+plot(xLinLSEE*ones(size(xPlot)), zPlot, 'LineWidth', 2, 'LineStyle','--')
+make_nice_figures(gcf, gca, 18, [], '$x$', '$f_X(x)$', 'PDF', [], [], [], []);
+legend('Prior', 'Posterior', 'Measurement', 'MAP', 'LSEE', 'Linear LSEE')
+
+
+
+% % Measurement error has speed-dependent variance
+% 
+% xSS	= linspace(0, 400, 100);
+% zSS	= linspace(-50, 400, 100);
+% 
+% infty = 1000;	% Practical "infinity"
+% 
+% [xMesh, yMesh]	= meshgrid(xSS, zSS);
+% 
+% fXPrior			= prior_X(xSS);
+% fYGivenX		= conditional_YGivenX(xSS(100), zSS);
+% fJoint			= joint_XY(xMesh, yMesh);
+% 
+% zStar			= 50;
+% xStar			= 60;
+% integral(@(x) posterior_XGivenY(x, zStar), xStar, infty)
+% % This is P[ X >= xBar | y = yMeas ]
+
+
+%% Sanity check; these should all be 1
 fprintf('\n\n ---- Sanity Check ---- \n')
-integral(@prior_X, 0, infty)
-integral(@(y) conditional_YGivenX(xSS(50), y), 0, infty)
-integral(@marginal_Y, -infty, infty)
+integral(@prior_X, 0, infty_)
+integral(@(y) conditional_ZGivenX(x1, y), 0, infty_)
+integral(@marginal_Y, -infty_, infty_)
 
 
 
-
-
-fontSize_ = 40;
-figure('units', 'normalized', 'OuterPosition', [0.05 0.05 0.8 0.9]);
-surf(xSS, ySS, fJoint, 'EdgeColor','none')
-xlabel('$x$', 'FontName', 'Times New Roman', ...
-	'FontSize', fontSize_, 'FontWeight', 'bold', 'interpreter', 'latex');
-ylabel('$y$', 'FontName', 'Times New Roman', ...
-	'FontSize', fontSize_, 'FontWeight', 'bold', 'interpreter', 'latex');
-ax = gca;
-ax.FontName = 'Times New Roman';
-ax.FontSize = fontSize_;
-
-
-	function pdf_ = conditional_YGivenX(x_, y_)
-		pdf_ = (1 ./ (sqrt(2*pi) .* (x_/10) ) ) .* ...
-			exp( -(y_ - x_).^2 ./ (2 .* (x_.^2 / 100) ) );
+	function pdf_ = conditional_ZGivenX(x_, z_)
+		pdf_ = (1 ./ (sqrt(2*pi) .* (alpha_*x_)) ) .* ...
+			exp( -0.5* ( (z_ - x_) ./ (alpha_*x_) ).^2 ); 
 	end
 
 	
 	function pdf_ = prior_X(x_)
-		pdf_ = (1/50) * exp(-x_ / 50);
+		pdf_ = lambda_ * exp(-lambda_*x_);
 	end
 
-	function pdf_ = joint_XY(x_, y_)
-		pdf_ = prior_X(x_) .* conditional_YGivenX(x_, y_);
+	function pdf_ = joint_XY(x_, z_)
+		pdf_ = prior_X(x_) .* conditional_ZGivenX(x_, z_);
 	end
 
-	function pdf_ = marginal_Y(y_)
-		pdf_ = zeros(size(y_));
-		for k = 1:numel(y_)
-			pdf_(k) = integral(@(x_) joint_XY(x_, y_(k)), 0, infty);
+	function pdf_ = marginal_Y(z_)
+		pdf_ = zeros(size(z_));
+		for k = 1:numel(z_)
+			pdf_(k) = integral(@(x_) joint_XY(x_, z_(k)), 0, infty_);
 		end
 	end
 
-	function pdf_ = posterior_XGivenY(x_, y_)
+	function pdf_ = posterior_X(x_, z_)
 		pdf_ = zeros(size(x_));
 		for k = 1:numel(x_)
-			pdf_(k) = joint_XY(x_(k), y_) ./ marginal_Y(y_);
+			pdf_(k) = joint_XY(x_(k), z_) ./ marginal_Y(z_);
 		end
 	end
 
