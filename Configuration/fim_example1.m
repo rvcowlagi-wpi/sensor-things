c8ea7853afa7960d7b146fe5c10ef5498605127a
rvcowlagi-wpi <129526829+rvcowlagi-wpi@users.noreply.github.com>
2024-04-16 05:55:06 -0400

    Update fim_example1.m

--- Configuration/fim_example1.m
+++ Configuration/fim_example1.m
@@ -19,7 +19,7 @@ plot(xPlot, yPlot1, 'LineWidth', 2);
 ax = gca; ax.ColorOrderIndex = ax.ColorOrderIndex - 1;
 plot(xPlot, yPlot2, 'LineWidth', 2);
 
-make_nice_figures(gcf, gca, 14, [], '$x_1$', '$x_2$', 'Target tracking', [], [], [-1 3.5], [-1 3.5])
+make_nice_figures(gcf, gca, 14, [], '$x_1$', '$x_2$', 'Fisher Information', [], [], [-1 3.5], [-1 3.5])
 
 
 %% Calculate FIM contours for one sensor
@@ -29,7 +29,8 @@ sensorMaxRange	= 2;
 rMeas			= (0.1)^2;
 
 % sensor2			= [2.9; 1.0];
-sensor2			= [2.25; 2.25];
+% sensor2			= [2.25; 2.25];
+sensor2			= [1.6; 1.6];
 
 nPoints = 100;
 x1F = (linspace(-1, 3.5, nPoints))';
@@ -80,6 +81,7 @@ ax = gca; ax.ColorOrderIndex = ax.ColorOrderIndex - 1;
 plot3(xPlot, yPlot2, zMax*ones(length(xPlot), 1), 'LineWidth', 2);
 
 plot3(maxFIMLocation(1), maxFIMLocation(2), zMax, '.', 'MarkerSize', 20)
+plot3(sensor2(1), sensor2(2), zMax, 'o', 'MarkerSize', 10)
 
 %(xHat(1) - x1F(m1))*(xHat(2) - x2F(m2))
 
@@ -127,9 +129,65 @@ plot(xPlot, yPlot1, 'LineWidth', 2);
 ax = gca; ax.ColorOrderIndex = ax.ColorOrderIndex - 1;
 plot(xPlot, yPlot2, 'LineWidth', 2);
 
-make_nice_figures(gcf, gca, 14, [], '$x_1$', '$x_2$', 'Target tracking', [], [], [-1 3.5], [-1 3.5])
+make_nice_figures(gcf, gca, 14, [], '$x_1$', '$x_2$', 'Mutual Information', [], [], [-1 3.5], [-1 3.5])
 
 surf(ax, x1F, x2F, MI, 'LineStyle','none');
 colorbar; view(2);
 plot3(maxMILocation(1), maxMILocation(2), zMax, '.', 'MarkerSize', 20)
+plot3(sensor2(1), sensor2(2), zMax, 'o', 'MarkerSize', 10)
+
+
+%% KL Divergence (2 sensors, 1 fixed)
+
+KLDPosterior = zeros(nPoints);
+maxKLD = 0;
+maxKLDLocation = [];
+
+for m1 = 1:nPoints
+	for m2 = 1:nPoints
+		expRange1	= norm([x1F(m1); x2F(m2)] - xHat);
+		expRange2	= norm(sensor2 - xHat);
+
+		C	= [...
+			(1 / expRange1)*[(xHat(1) - x1F(m1))		(xHat(2) - x2F(m2)) ]; ...
+			(1 / expRange2)*[(xHat(1) - sensor2(1))		(xHat(2) - sensor2(2)) ] ...
+			];
+		PZZ	= C * PXX * C' + rMeas*eye(2);
+		PXZ	= PXX*C';
+		L	= PXZ / PZZ;
+		postP	= (eye(2) - L*C)*PXX;
+
+		if (expRange1 <= sensorMaxRange) && (expRange1 >= sensorMinRange)
+			KLDPosterior(m2, m1) = 0.5*( log( det(postP) / det(PXX) ) - 2 + ...
+				trace((L'/postP)*L*PZZ) + trace(postP\PXX) );
+		end
+
+		if KLDPosterior(m2, m1) >= maxKLD
+			maxKLD = KLDPosterior(m2, m1);
+			maxKLDLocation = [x1F(m1); x2F(m2)];
+		end
+	end
+end
+
+
+
+zMax	= max(KLDPosterior(:));
+
+
+figure;
+plot(xHat(1), xHat(2), 'x', 'MarkerSize', 10); hold on; axis equal
+xPlot	= linspace( (xHat(1) - (1-1E-6)*sqrt(PXX(1,1))), (xHat(1) + (1-1E-6)*sqrt(PXX(1,1))), 100 );
+yPlot1	= sqrt(PXX(2,2)) * (1 - (xPlot - xHat(1)).^2 / PXX(1,1) ).^0.5 + xHat(2);
+yPlot2	= -sqrt(PXX(2,2)) * (1 - (xPlot - xHat(1)).^2 / PXX(1,1) ).^0.5 + xHat(2);
+
+plot(xPlot, yPlot1, 'LineWidth', 2);
+ax = gca; ax.ColorOrderIndex = ax.ColorOrderIndex - 1;
+plot(xPlot, yPlot2, 'LineWidth', 2);
+
+make_nice_figures(gcf, gca, 14, [], '$x_1$', '$x_2$', 'KL Divergence', [], [], [-1 3.5], [-1 3.5])
+
+surf(ax, x1F, x2F, KLDPosterior, 'LineStyle','none');
+colorbar; view(2);
+plot3(maxKLDLocation(1), maxKLDLocation(2), zMax, '.', 'MarkerSize', 20)
+plot3(sensor2(1), sensor2(2), zMax, 'o', 'MarkerSize', 10)
 
